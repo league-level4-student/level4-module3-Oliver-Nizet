64f48c3f026eb4ebe60e2c502cc855b35896c9e9
Sportsdawg1 <league@WorthyMini-6.attlocal.net>
2019-01-15 17:49:57 -0800

    finished module 3

--- src/_02_gotta_catchem_all/ExceptionMethods.java
+++ src/_02_gotta_catchem_all/ExceptionMethods.java
@@ -1,5 +1,20 @@
 package _02_gotta_catchem_all;
 
 public class ExceptionMethods {
-	
+	public static double divide(double n1, double n2) throws Exception {
+		if(n2==0.0) {
+			throw new Exception();
+		}
+		return(n1/n2);
+	}
+	public static String reverseString(String s) throws Exception {
+		if(s.length()==0) {
+			throw new Exception();
+		}
+		String a = "";
+		for(int i = s.length()-1; i >= 0; i--) {
+			a += s.charAt(i);
+		}
+		return a;
+	}
 }
